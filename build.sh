2ef03cf194057b51e3e85fd49a6959f3a0b9fc5a
developer <developer@devops.lab>
2021-10-08 23:11:57 +0300

    remove docker net host from build, use node bin instead

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ set -xeof pipefail
 # 2) ansible-version ~2.9.6 with Python interpreter ~3.8.10 (tested)
 # 3) password-less authentication with the builder host (localhost)
 # 4) runtime user needs to be in a docker group
+# 5) node - https://nodejs.org/download/release/v8.4.0/
 
 BASEDIR="$(readlink -f $(dirname $0))"
 REGISTRY_URL="${REGISTRY_URL:-vnox91}"
@@ -40,3 +41,18 @@ ansible-playbook -i "${BASEDIR}/ansible/inventory/hosts.ini" \
 
 # Bonus: application log shipping to a dedicated central file located on the node with fluentd (ok for a single node cluster, not ok for else...)
 # kubectl apply -f fluentd/fluentd-daemonset.yaml
+
+
+# Install node
+# cat<<EOF > /tmp/setup_npm.sh
+# #!/bin/bash
+# set -eoxf pipefail
+# NODE_VERSION="node-v8.4.0-linux-x64"
+# sudo mkdir -p "/opt/\${NODE_VERSION}"
+# sudo wget "https://nodejs.org/download/release/v8.4.0/\${NODE_VERSION}.tar.gz" -O "/opt/\${NODE_VERSION}/\${NODE_VERSION}.tar.gz"
+# pushd /opt/\${NODE_VERSION}
+# sudo tar -xf \${NODE_VERSION}.tar.gz --strip-components=1
+# sudo ln -s /opt/\${NODE_VERSION}/bin/node /usr/bin/node
+# popd
+# sudo rm -f /opt/\${NODE_VERSION:-none}/\${NODE_VERSION:-none}.tar.gz
+# EOF

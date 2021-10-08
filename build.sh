40bb3a5527cd6afaee100c715e92eb761b0e62d6
developer <developer@devops.lab>
2021-10-08 21:32:40 +0300

    comments update

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -xeof pipefail
 
-# Please make sure to specify the following, before running:
+# Please make sure to specify the following before running:
 # export REGISTRY_URL=[...]
 
 # Prerequisites:
@@ -11,20 +11,32 @@ set -xeof pipefail
 # 3) password-less authentication with the builder host (localhost)
 # 4) runtime user needs to be in a docker group
 
-# For Pizza-express Chart deployment:
-# export DOMAIN=[...]
-# export PIZZA_IMAGE="${REGISTRY_URL}/pizza-express"
-# helm install pizza-express ./Charts/pizza-express \
-#    --set image.repository="${PIZZA_IMAGE}" \
-#    --set "ingress.hosts[0].host=${DOMAIN},ingress.hosts[0].paths[0].path=/"
-
 BASEDIR="$(readlink -f $(dirname $0))"
 REGISTRY_URL="${REGISTRY_URL:-vnox91}"
 APP_NAME="pizza-express"
 APP_TAG=${PIZZA_APP_TAG:-"v0.0.1"}
 
+
+# the following will execute application tests, build the container, tag and push it
+# into REGISTRY_URL/APP_NAME:APP_TAG location
 export ANSIBLE_CONFIG="${BASEDIR}/ansible.cfg"
 ansible-playbook -i "${BASEDIR}/ansible/inventory/hosts.ini" \
     "${BASEDIR}/ansible/playbooks/main.yaml" -e app_dir="${BASEDIR}" \
     -e app_image="${REGISTRY_URL}/${APP_NAME}" \
     -e app_tag="${APP_TAG}"
+
+
+# Pizza-express Chart deployment:
+# Prerequisites:
+# 1) Kind cluster
+# 2) helm
+
+# export DOMAIN=[...]
+# export REGISTRY_URL=[...]
+# export PIZZA_IMAGE="${REGISTRY_URL}/pizza-express"
+# helm install pizza-express ./Charts/pizza-express \
+#    --set image.repository="${PIZZA_IMAGE}" \
+#    --set "ingress.hosts[0].host=${DOMAIN},ingress.hosts[0].paths[0].path=/"
+
+# Bonus: application log shipping to a dedicated central file located on the node with fluentd (ok for a single node cluster, not ok for else...)
+# kubectl apply -f fluentd/fluentd-daemonset.yaml
